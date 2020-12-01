3eef60eead049498079880d64fdf8ea9d3815849
yochaigal <yochaigal@gmail.com>
2020-12-01 17:57:47 -0500

    added armor values

--- js/main.js
+++ js/main.js
@@ -19,6 +19,20 @@ for (let i = 0; i < allItems.length; i++) {
     total++;
 }
 }
+
+let armorTotal = 0;
+let armorValue = [];
+armorValue.push(armor, helmet);
+armorValue.forEach((item) => {
+  if (item.includes("1")) {
+    armorTotal++;
+  } else if (item.includes("2")) {
+    armorTotal += 2;
+  } else if (item.includes("3")) {
+    armorTotal += 3;
+  }
+});
+
 console.log(total);
 $("#character").html(generate_text("character"));
 $("#age").html(roll(20) + roll(20) + 10);
@@ -33,5 +47,6 @@ $("#tool").html(tool);
 $("#gear").html(gear);
 $("#trinket").html(trinket);
 $("#bonus").html(bonus);
+$("#armorTotal").html(armorTotal);
 $("#total").html(total);
 $("#gold").html(roll(6) + roll(6) + roll(6));
